d0c58e1e1675b8677c7cffcd24c8b4ec7dbcd45c
이현우 <@LAPTOP-RSJUIVVM>
2024-01-24 17:48:08 +0900

    인터페이스

---
+++ testexam/src/CodingTest1/wordbookfunction.java
@@ -0,0 +1,11 @@
+package CodingTest1;
+
+public interface wordbookfunction {
+	public void Menu();
+	public void addWord();
+	public void searchWord();
+	public void deleteWord();
+	public void test();
+	public void wordList();
+	public void exit();
+}
